db0e843c4143b61b825b1e1d76edd9d18be039ef
Mehul Lakhatariya <mehul8055@gmail.com>
2020-02-26 11:12:13 +0530

    Added actuator

--- number-service/src/main/java/com/mehul/training/controller/NumberController.java
+++ number-service/src/main/java/com/mehul/training/controller/NumberController.java
@@ -1,22 +1,27 @@
 package com.mehul.training.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mehul.training.domain.Number;
-import com.mehul.training.service.NumberService;
 
+
+@RefreshScope
 @RestController
 public class NumberController {
 
-	@Autowired
-	private NumberService service;
+	@Value("${min}")
+	private int min;
+
+	@Value("${max}")
+	private int max;
 
 	@GetMapping("/data")
 	public ResponseEntity<Number> getNumber(){
-		return new ResponseEntity<Number>(service.getNumber(), HttpStatus.OK);
+		return new ResponseEntity<Number>(new Number(min, max), HttpStatus.OK);
 	}
 }
--- number-service/src/main/java/com/mehul/training/service/NumberService.java
+++ number-service/src/main/java/com/mehul/training/service/NumberService.java
@@ -1,18 +1,22 @@
 package com.mehul.training.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
-import com.mehul.training.NumberConfig;
 import com.mehul.training.domain.Number;
 
 @Service
+@RefreshScope
 public class NumberService {
 
-	@Autowired
-	private NumberConfig config;
+	@Value("${min}")
+	private int min;
+
+	@Value("${max}")
+	private int max;
 
 	public Number getNumber() {
-		return new Number(config.getMin(), config.getMax());
+		return new Number(min, max);
 	}
 }
