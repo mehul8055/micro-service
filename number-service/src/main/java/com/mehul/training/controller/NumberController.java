12b339fe02a5c6f05ab3da6c4e69d29fab916036
Mehul Lakhatariya <mehul8055@gmail.com>
2020-02-26 10:32:14 +0530

    Modified some properties

--- number-service/src/main/java/com/mehul/training/controller/NumberController.java
+++ number-service/src/main/java/com/mehul/training/controller/NumberController.java
@@ -3,13 +3,13 @@ package com.mehul.training.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.mehul.training.domain.Number;
 import com.mehul.training.service.NumberService;
 
-@Controller
+@RestController
 public class NumberController {
 
 	@Autowired
